032639b8532e062a22fb067f123f1df9957b9421
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-09-06 02:58:17 +0600

    module finished

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,25 +1,32 @@
-import BlogDetails from '@/components/ui/BlogDetails';
-import React from 'react';
+import BlogDetails from "@/components/ui/BlogDetails";
+import { TBlog } from "@/types";
+import React from "react";
 
 type BlogId = {
-    params: {
-        blogId: string
-    }
-}
+  params: {
+    blogId: string;
+  };
+};
 
-export const generate 
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: TBlog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const BlogDetailsPage = async ({ params }: BlogId) => {
-    const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
-        cache: "no-store"
-    })
-    const blog = await res.json();
-    console.log(blog)
-    return (
-        <div className='my-10'>
-            <BlogDetails key={blog.id} blog={blog} />
-        </div>
-    );
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+  console.log(blog);
+  return (
+    <div className="my-10">
+      <BlogDetails key={blog.id} blog={blog} />
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
